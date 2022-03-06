7316edcb8586f62a6e2fc0cfdc6b47bd2d804015
DonggooLee <ehdrn0811@gmail.com>
2022-03-06 17:52:45 +0900

    chapter01_begin

---
+++ chapter01_begin/src/org/ddongq/ex/Ex01_HelloWorld.java
@@ -0,0 +1,13 @@
+package org.ddongq.ex;
+
+public class Ex01_HelloWorld {							// 클래스 (소스 파일의 이름과 같다.)
+	public static void main(String[] args) {			
+		// 주석 : 실행과 상관없이 코드에 설명을 붙인 것.
+		// 1. //	: //이후 라인 끝까지 주석으로 인지
+		// 2. /* */ : 사이에 있는 모든 범위를 주석으로 인지
+		
+		System.out.println("Hello World");
+		//	System.out.println("Hello World");
+		/*	System.out.println("Hello World");	*/
+	}
+}
