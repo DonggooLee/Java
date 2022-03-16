76aa5924428f982a1430f97a88aad7d1fd9a1e9e
Donggoo <ehdrn0811@gmail.com>
2022-03-16 23:58:41 +0900

    chapter10_static final

---
+++ chapter10_staatic final/ex/Ex01_StatiSample.java
@@ -0,0 +1,24 @@
+package org.ddongq.ex;
+
+import javax.swing.JOptionPane;
+
+public class Ex01_StatiSample {
+	
+	static final double PI = Math.PI;					// 누구나 사용할 수 있는 전역변수 Math <= 클래스
+	
+	public static void main(String[] args) {
+
+		// 그동안 사용한 static 멤버들
+		System.out.println(); 							// System <= 클래스, out <= 패키지 파일
+		JOptionPane.showInputDialog("입력메세지");			// JOptionPane <= 클래스, showInputDialog <= 메소드
+		JOptionPane.showMessageDialog(null, "Hello");	// JOptionPane <= 클래스, showMessageDialog <= 메소드
+		
+		// 공통점
+		// new 가 없다. -> 객체 생성 안한다.
+		
+//		PI = 123;										// 위에 static final 로 고정했기 때문에 변경 불가능
+//		System.out.println(PI);
+		
+	}
+	
+}
